0d1c4052d549959248b150f944bf426a14ff8eb3
Laurent ARNAL <laurent@clae.net>
2026-01-01 16:19:34 +0100

    update metadata
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.audio/src/main/java/org/openhab/binding/audio/internal/handler/AudioHandler.java
+++ bundles/org.openhab.binding.audio/src/main/java/org/openhab/binding/audio/internal/handler/AudioHandler.java
@@ -12,12 +12,13 @@
  */
 package org.openhab.binding.audio.internal.handler;
 
-import static org.openhab.binding.audio.internal.AudioBindingConstants.VOLUME;
+import static org.openhab.binding.audio.internal.AudioBindingConstants.*;
 
 import java.io.IOException;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.audio.internal.AudioBindingConstants;
 import org.openhab.core.audio.AudioManager;
 import org.openhab.core.audio.AudioSink;
 import org.openhab.core.audio.JavaSoundAudioSink;
@@ -25,8 +26,11 @@ import org.openhab.core.audio.URLAudioStream;
 import org.openhab.core.audio.WebAudioAudioSink;
 import org.openhab.core.library.types.MediaCommandEnumType;
 import org.openhab.core.library.types.MediaCommandType;
+import org.openhab.core.library.types.MediaStateType;
 import org.openhab.core.library.types.PercentType;
+import org.openhab.core.library.types.PlayPauseType;
 import org.openhab.core.library.types.StringType;
+import org.openhab.core.media.MediaListenner;
 import org.openhab.core.media.MediaService;
 import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.Thing;
@@ -53,6 +57,10 @@ public class AudioHandler extends BaseThingHandler {
     private final MediaService mediaService;
     private @NonNullByDefault({}) AudioManager audioManager;
 
+    private String artistName = "";
+    private String albumName = "";
+    private String trackName = "";
+
     public AudioHandler(Thing thing, AudioSink audioSink, MediaService mediaService, AudioManager audioManager) {
         super(thing);
 
@@ -75,6 +83,32 @@ public class AudioHandler extends BaseThingHandler {
 
     @Override
     public void dispose() {
+    }
+
+    private void updateControlState() {
+        PlayPauseType state = PlayPauseType.NONE;
+        if ("PLAYING".equals("PLAYING")) {
+            state = PlayPauseType.PLAY;
+        } else if ("STOPPED".equals("")) {
+            state = PlayPauseType.PAUSE;
+        } else if ("PAUSED_PLAYBACK".equals("")) {
+            state = PlayPauseType.PAUSE;
+        }
+
+        double trackPosition = 0;
+        double trackDuration = 0;
+
+        MediaStateType mediaStateType = new MediaStateType(state, new StringType(this.getThing().getUID().getId()),
+                new StringType(AudioBindingConstants.BINDING_ID));
+
+        mediaStateType.setCurrentPlayingPosition(trackPosition * 1000.00);
+        mediaStateType.setCurrentPlayingTrackDuration(trackDuration * 1000.00);
+        mediaStateType.setCurrentPlayingArtistName(artistName);
+        // mediaStateType.set(artistName);
+        mediaStateType.setCurrentPlayingTrackName(trackName);
+        // mediaStateType.setCurrentPlayingArtUri(artUri);
+        // mediaStateType.setCurrentPlayingVolume(getCurrentVolume().doubleValue());
+        updateState(CONTROL, mediaStateType);
     }
 
     @Override
@@ -104,12 +138,22 @@ public class AudioHandler extends BaseThingHandler {
             String val = mediaType.getParam().toFullString();
 
             if (commandType == MediaCommandEnumType.PLAY) {
-                String streamUri = mediaService.getMediaListenner("tidal").getStreamUri(val);
+                MediaListenner mediaListenner = mediaService.getMediaListenner("6DB0E5AF-63C3-4901-B792-504CE4935B05");
+                String streamUri = mediaListenner.getStreamUri(val);
                 logger.info("Stream uri is:{}", streamUri);
+
+                albumName = "album";
+                artistName = "artist";
+                trackName = val;
+                updateState(TITLE, new StringType(trackName));
+                updateState(ALBUM, new StringType(albumName));
+                updateState(ARTIST, new StringType(artistName));
+                updateControlState();
+
                 if (!streamUri.isBlank()) {
                     try {
                         URLAudioStream urlAudioStream = new URLAudioStream(streamUri);
-                        audioManager.play(urlAudioStream);
+                        audioManager.play(urlAudioStream, this.audioSink.getId());
 
                     } catch (Exception ex) {
 
