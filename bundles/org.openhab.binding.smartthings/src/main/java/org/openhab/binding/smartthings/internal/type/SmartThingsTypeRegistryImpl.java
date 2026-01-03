d06d7e3f7b6226cf0e4e61b3143b75032b54a0d1
Laurent ARNAL <laurent@clae.net>
2026-01-03 16:05:32 +0100

    update header for 2026 Copyright
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/type/SmartThingsTypeRegistryImpl.java
+++ bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/type/SmartThingsTypeRegistryImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2026 Contributors to the openHAB project
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -249,6 +249,7 @@ public class SmartThingsTypeRegistryImpl implements SmartThingsTypeRegistry {
                 lcChannelTypeProvider.addChannelType(channelType);
             }
         }
+
     }
 
     private ChannelType createChannelType(SmartThingsCapability capa, @Nullable SmartThingsProperty unit,
